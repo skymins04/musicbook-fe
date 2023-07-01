356a105a3927441755fd61ce2c224619ade3c20d
Kang Minsu <skymin0417@gmail.com>
2023-07-01 22:58:53 +0900

    fix: add duration to PopMenu components

--- src/components/PopMenu/PopMenu.tsx
+++ src/components/PopMenu/PopMenu.tsx
@@ -10,7 +10,7 @@ export const PopMenu = ({ className, children }: PopMenuProps) => {
   return (
     <div
       className={classNames(
-        "flex flex-col items-stretch justify-center gap-10 rounded-12 bg-white py-10 shadow-md dark:bg-gray-700",
+        "flex flex-col items-stretch justify-center gap-10 rounded-12 bg-white py-10 shadow-md duration-200 dark:bg-gray-700",
         className
       )}
     >
