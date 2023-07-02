044e89a74e67c5b2c3186073ea35e183d7fac5b4
Kang Minsu <skymin0417@gmail.com>
2023-07-02 19:51:51 +0900

    feat: add skeleton mode to Avatar, ProfileImage components

--- src/components/Avatar/Avatar.tsx
+++ src/components/Avatar/Avatar.tsx
@@ -2,20 +2,32 @@ import { DetailsHTMLAttributes, useState } from "react";
 import classNames from "classnames";
 import { ProfileImage } from "../ProfileImage";
 import { AspectRatio } from "../AspectRatio";
+import { SkeletonBaseProps } from "../Skeleton";
 
 export type AvatarProps = {
   src: string;
-} & DetailsHTMLAttributes<HTMLButtonElement>;
+} & SkeletonBaseProps &
+  DetailsHTMLAttributes<HTMLButtonElement>;
 
-export const Avatar = ({ className, children, src, ...props }: AvatarProps) => {
+export const Avatar = ({
+  className,
+  children,
+  src,
+  isShow,
+  ...props
+}: AvatarProps) => {
   const [isError, setIsError] = useState(false);
 
   return (
     <button
       {...props}
-      className={classNames("group relative min-w-[20px]", className)}
+      className={classNames("group relative min-h-max min-w-max", className)}
     >
-      <ProfileImage src={src} onError={() => setIsError(true)} />
+      <ProfileImage
+        isShow={isShow}
+        src={src}
+        onError={() => setIsError(true)}
+      />
       {!isError && (
         <AspectRatio
           className="!absolute left-0 top-0 h-full w-full rounded-full duration-200 group-hover:bg-blackAlpha-200"
