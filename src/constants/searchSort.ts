00b4ba35ddf09029238d0686f2f51b91ae821a73
Kang Minsu <skymin0417@gmail.com>
2023-08-11 22:53:33 +0900

    feat: add searchSort constant

---
+++ src/constants/searchSort.ts
@@ -0,0 +1,7 @@
+import { GETMusicSearchSort } from "@apis";
+
+export const SEARCH_SORT_LABEL: Record<GETMusicSearchSort, string> = {
+  NEWEST: "최근올라온수록곡",
+  POPULAR: "인기수록곡",
+  SUGGEST: "추천",
+};
