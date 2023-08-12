da5f7e416da4a23ae58868002ca1bd907603b664
Kang Minsu <skymin0417@gmail.com>
2023-08-13 02:29:48 +0900

    fix: add release style to CardLayout

--- src/components/CardLayout/CardLayout.tsx
+++ src/components/CardLayout/CardLayout.tsx
@@ -10,7 +10,7 @@ export const CardLayout = ({ children, className }: CardLayoutProps) => {
   return (
     <div
       className={classNames(
-        "grid grid-cols-[repeat(auto-fill,minmax(170px,1fr))] flex-col gap-20 tablet:grid-cols-[repeat(auto-fill,minmax(150px,1fr))] semi-tablet:flex semi-tablet:gap-12",
+        "relative grid grid-cols-[repeat(auto-fill,minmax(170px,1fr))] flex-col gap-20 tablet:grid-cols-[repeat(auto-fill,minmax(150px,1fr))] semi-tablet:flex semi-tablet:gap-12",
         className
       )}
     >
