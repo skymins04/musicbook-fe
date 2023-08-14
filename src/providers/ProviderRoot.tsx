a353adacc3ce72fb38aeefff1b13037a895a0036
Kang Minsu <skymin0417@gmail.com>
2023-08-15 00:52:48 +0900

    feat: add lazy loading to ProviderRoot component

--- src/providers/ProviderRoot.tsx
+++ src/providers/ProviderRoot.tsx
@@ -1,14 +1,23 @@
 import { ReactNode } from "react";
 import dynamic from "next/dynamic";
-import { SWRConfig } from "swr/_internal";
-import { RouterProgressProvider, ToastProvider } from "./globalProviders";
 
+const SWRConfig = dynamic(() =>
+  import("swr/_internal").then((mod) => mod.SWRConfig)
+);
 const GlobalDialogProvider = dynamic(() =>
   import("./globalProviders/DialogProvider").then((mod) => mod.DialogProvider)
 );
 const SearchSpotlight = dynamic(() =>
   import("@components/Search/SearchSpotlight/SearchSpotlight").then(
     (mod) => mod.SearchSpotlight
+  )
+);
+const ToastProvider = dynamic(() =>
+  import("./globalProviders/ToastProvider").then((mod) => mod.ToastProvider)
+);
+const RouterProgressProvider = dynamic(() =>
+  import("./globalProviders/RouterProgressProvider").then(
+    (mod) => mod.RouterProgressProvider
   )
 );
 
