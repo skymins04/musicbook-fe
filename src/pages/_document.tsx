2d298745c34c5d9e63709de27f634e760167278e
Kang Minsu <skymin0417@gmail.com>
2023-08-15 01:31:06 +0900

    feat: add lang attribute to html tag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,7 @@ import { Head, Html, Main, NextScript } from "next/document";
 export default function Document(ctx: any) {
   const { theme } = ctx.__NEXT_DATA__.props.pageProps;
   return (
-    <Html>
+    <Html lang="ko">
       <Head />
       <body className={theme}>
         <Main />
