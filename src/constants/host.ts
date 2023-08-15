c8a15fccb1a586b9566e54287fe0c5d9c18e8c32
Kang Minsu <skymin0417@gmail.com>
2023-08-15 15:07:31 +0900

    feat: add HOST constant

--- src/constants/host.ts
+++ src/constants/host.ts
@@ -1,4 +1,5 @@
 export const DOMAIN = process.env.DOMAIN || "musicbook.dev";
 export const DOT_DOMAIN = `.${DOMAIN}`;
+export const HOST = `https://${DOMAIN}`;
 export const API_HOST = `https://api.${DOMAIN}`;
 export const ASSET_HOST = `https://assets.${DOMAIN}`;
