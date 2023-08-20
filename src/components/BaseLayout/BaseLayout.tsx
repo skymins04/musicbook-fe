791c28744aa2d623cfbb38d0624640901eee28e4
Kang Minsu <skymin0417@gmail.com>
2023-08-21 03:28:01 +0900

    feat: edit BaseLayout component to use UserContextProvider component

--- src/components/BaseLayout/BaseLayout.tsx
+++ src/components/BaseLayout/BaseLayout.tsx
@@ -4,40 +4,45 @@ import { useBoolean } from "@hooks";
 import { ReactNode } from "react";
 import { BaseLayoutMobileDrawer } from "./BaseLayoutMobileDrawer";
 import dynamic from "next/dynamic";
+import { User } from "@apis";
+import { UserContextProvider } from "@providers";
 
 const TopNotice = dynamic(
   import("../TopNotice/TopNotice").then((mod) => mod.TopNotice)
 );
 
 export type BaseLayoutProps = {
+  user?: User;
   children: ReactNode;
 };
 
-export const BaseLayout = ({ children }: BaseLayoutProps) => {
+export const BaseLayout = ({ user, children }: BaseLayoutProps) => {
   const [isOpenNotice, setIsOpenNotice] = useBoolean(false);
 
   return (
-    <div
-      className={classNames(
-        "relative flex h-full w-full flex-col items-stretch justify-stretch bg-gray-100 duration-200 dark:bg-gray-800"
-      )}
-    >
-      <TopNotice onOpen={setIsOpenNotice.on} onClose={setIsOpenNotice.off} />
-      <Header className="max-h-[56px] min-h-[56px] shadow-sm" />
+    <UserContextProvider user={user}>
       <div
         className={classNames(
-          "relative flex items-stretch justify-stretch",
-          isOpenNotice ? "h-[calc(100vh-56px-28px)]" : "h-[calc(100vh-56px)]"
+          "relative flex h-full w-full flex-col items-stretch justify-stretch bg-gray-100 duration-200 dark:bg-gray-800"
         )}
       >
-        <div className="relative block h-full min-w-max overflow-y-auto overflow-x-hidden border-r-1 border-gray-200 bg-white duration-200 dark:border-gray-800 dark:bg-gray-700 mobile:hidden">
-          <MainDrawer isAllowMinified isShow />
+        <TopNotice onOpen={setIsOpenNotice.on} onClose={setIsOpenNotice.off} />
+        <Header className="max-h-[56px] min-h-[56px] shadow-sm" />
+        <div
+          className={classNames(
+            "relative flex items-stretch justify-stretch",
+            isOpenNotice ? "h-[calc(100vh-56px-28px)]" : "h-[calc(100vh-56px)]"
+          )}
+        >
+          <div className="relative block h-full min-w-max overflow-y-auto overflow-x-hidden border-r-1 border-gray-200 bg-white duration-200 dark:border-gray-800 dark:bg-gray-700 mobile:hidden">
+            <MainDrawer isAllowMinified isShow />
+          </div>
+          <div className="relative box-border h-full w-full overflow-y-auto overflow-x-hidden">
+            {children}
+          </div>
+          <BaseLayoutMobileDrawer />
         </div>
-        <div className="relative box-border h-full w-full overflow-y-auto overflow-x-hidden">
-          {children}
-        </div>
-        <BaseLayoutMobileDrawer />
       </div>
-    </div>
+    </UserContextProvider>
   );
 };
