b772cc001e31dd8f489999e400e1b736b79df220
Kang Minsu <skymin0417@gmail.com>
2023-08-29 20:12:42 +0900

    feat: init yarn pnp & add preact

--- next.config.js
+++ next.config.js
@@ -11,6 +11,17 @@ const nextConfig = {
     RELEASE_ENV: process.env.RELEASE_ENV,
     DOMAIN: process.env.DOMAIN,
   },
+  webpack: (config, { dev, isServer }) => {
+    if (!dev && !isServer) {
+      Object.assign(config.resolve.alias, {
+        "react/jsx-runtime.js": "preact/compat/jsx-runtime",
+        react: "preact/compat",
+        "react-dom/test-utils": "preact/test-utils",
+        "react-dom": "preact/compat",
+      });
+    }
+    return config;
+  },
 };
 
 module.exports = nextConfig;
